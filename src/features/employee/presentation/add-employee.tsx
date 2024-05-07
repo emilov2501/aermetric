dc69d737a04aaf7ed624a0e4b26c078502093391
Eldiiar <emilov2501@gmail.com>
2024-05-07 21:22:55 +0600

    feat: add a employee form

--- src/features/employee/presentation/add-employee.tsx
+++ src/features/employee/presentation/add-employee.tsx
@@ -1,13 +1,27 @@
+"use client";
+
 import { Button } from "@/shared/ui/button";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/shared/ui/dialog";
-import { Plus } from "lucide-react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/shared/ui/form";
+import { Input } from "@/shared/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { useCreateEmployeeMutation } from "../data/employee.api";
 
 export const AddEmployee = () => {
@@ -16,22 +30,136 @@ export const AddEmployee = () => {
   return (
     <Dialog>
       <DialogTrigger>
-        <Button variant="outline">
-          <div className="flex gap-1 items-center uppercase">
-            <Plus size={18} />
-            Add Employee
-          </div>
+        <Button asChild variant="outline">
+          <div className="flex gap-1 items-center uppercase">Add employee</div>
         </Button>
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </DialogDescription>
+          <DialogTitle>Craete a new employee</DialogTitle>
         </DialogHeader>
+        <EmployeeForm onSubmit={(data) => console.log(data)} />
+        <DialogFooter>
+          <Button type="submit" form="employee-form">
+            Create
+          </Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
 };
+
+type UseEmployeeForm = z.infer<typeof formSchema>;
+
+interface EmployeeFormProps {
+  onSubmit: (data: UseEmployeeForm) => void;
+}
+
+const EmployeeForm: React.FC<EmployeeFormProps> = ({ onSubmit }) => {
+  const form = useForm<UseEmployeeForm>({
+    resolver: zodResolver(formSchema),
+    values: {
+      name: "",
+      email: "",
+      age: "",
+      position: "",
+      department: "",
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8"
+        id="employee-form"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="Johh Doe" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="example@mail.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="age"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Age</FormLabel>
+              <FormControl>
+                <Input type="number" placeholder="30" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="position"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Position</FormLabel>
+              <FormControl>
+                <Input placeholder="Developer" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="department"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Department</FormLabel>
+              <FormControl>
+                <Input placeholder="Development" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+};
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  email: z.string().email().min(2, {
+    message: "Email must be at least 2 characters.",
+  }),
+  age: z.coerce.string().min(2),
+  position: z.string().min(2, {
+    message: "Position must be at least 2 characters.",
+  }),
+  department: z.string().min(2, {
+    message: "Department must be at least 2 characters.",
+  }),
+});
