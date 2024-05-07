6c774baf90b801306194e93e9f17db8515fbddd6
Eldiiar <emilov2501@gmail.com>
2024-05-07 21:25:39 +0600

    refactor: employee form

--- src/features/employee/presentation/add-employee.tsx
+++ src/features/employee/presentation/add-employee.tsx
@@ -9,20 +9,8 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/shared/ui/dialog";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/shared/ui/form";
-import { Input } from "@/shared/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
-import React from "react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
 import { useCreateEmployeeMutation } from "../data/employee.api";
+import { EmployeeForm } from "./employee-form";
 
 export const AddEmployee = () => {
   const add = useCreateEmployeeMutation();
@@ -30,7 +18,7 @@ export const AddEmployee = () => {
   return (
     <Dialog>
       <DialogTrigger>
-        <Button asChild variant="outline">
+        <Button asChild variant="secondary">
           <div className="flex gap-1 items-center uppercase">Add employee</div>
         </Button>
       </DialogTrigger>
@@ -48,118 +36,3 @@ export const AddEmployee = () => {
     </Dialog>
   );
 };
-
-type UseEmployeeForm = z.infer<typeof formSchema>;
-
-interface EmployeeFormProps {
-  onSubmit: (data: UseEmployeeForm) => void;
-}
-
-const EmployeeForm: React.FC<EmployeeFormProps> = ({ onSubmit }) => {
-  const form = useForm<UseEmployeeForm>({
-    resolver: zodResolver(formSchema),
-    values: {
-      name: "",
-      email: "",
-      age: "",
-      position: "",
-      department: "",
-    },
-  });
-
-  return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="space-y-8"
-        id="employee-form"
-      >
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="Johh Doe" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="example@mail.com" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="age"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Age</FormLabel>
-              <FormControl>
-                <Input type="number" placeholder="30" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="position"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Position</FormLabel>
-              <FormControl>
-                <Input placeholder="Developer" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="department"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Department</FormLabel>
-              <FormControl>
-                <Input placeholder="Development" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-      </form>
-    </Form>
-  );
-};
-
-const formSchema = z.object({
-  name: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-  email: z.string().email().min(2, {
-    message: "Email must be at least 2 characters.",
-  }),
-  age: z.coerce.string().min(2),
-  position: z.string().min(2, {
-    message: "Position must be at least 2 characters.",
-  }),
-  department: z.string().min(2, {
-    message: "Department must be at least 2 characters.",
-  }),
-});
---
+++ src/features/employee/presentation/employee-form.tsx
@@ -0,0 +1,127 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/shared/ui/form";
+import { Input } from "@/shared/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+type UseEmployeeForm = z.infer<typeof formSchema>;
+
+interface EmployeeFormProps {
+  onSubmit: (data: UseEmployeeForm) => void;
+}
+
+export const EmployeeForm: React.FC<EmployeeFormProps> = ({ onSubmit }) => {
+  const form = useForm<UseEmployeeForm>({
+    resolver: zodResolver(formSchema),
+    values: {
+      name: "",
+      email: "",
+      age: "",
+      position: "",
+      department: "",
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8"
+        id="employee-form"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="Johh Doe" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="example@mail.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="age"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Age</FormLabel>
+              <FormControl>
+                <Input type="number" placeholder="30" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="position"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Position</FormLabel>
+              <FormControl>
+                <Input placeholder="Developer" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="department"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Department</FormLabel>
+              <FormControl>
+                <Input placeholder="Development" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+};
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  email: z.string().email().min(2, {
+    message: "Email must be at least 2 characters.",
+  }),
+  age: z.coerce.string().min(2),
+  position: z.string().min(2, {
+    message: "Position must be at least 2 characters.",
+  }),
+  department: z.string().min(2, {
+    message: "Department must be at least 2 characters.",
+  }),
+});
