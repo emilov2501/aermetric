d27637127f02c35335e3a0876364d82376ea35a2
Eldiiar <emilov2501@gmail.com>
2024-05-08 00:49:19 +0600

    fix: modal

--- src/features/employee/presentation/delete-choose-employees.tsx
+++ src/features/employee/presentation/delete-choose-employees.tsx
@@ -2,36 +2,70 @@
 
 import { useAppDispatch, useAppSelector } from "@/config/store";
 import { Button } from "@/shared/ui/button";
-import { useCallback, useMemo } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/shared/ui/dialog";
+
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useDeleteEmployeeMutation } from "../data/employee.api";
 import { setEmployee } from "../data/employee.slice";
 
 export const DeleteChooseEmployees = () => {
+  const [open, setOpen] = useState(false);
   const { checked } = useAppSelector((state) => state.employee);
   const dispatch = useAppDispatch();
   const [deleteEmployee] = useDeleteEmployeeMutation();
 
+  const items = useMemo(() => Object.values(checked), [checked]);
+
   const handleDelete = useCallback(() => {
-    const items = Object.values(checked);
     items.forEach((item) => {
       deleteEmployee(item.id);
       dispatch(setEmployee(item));
     });
-  }, [checked]);
+
+    setOpen(false);
+  }, [items]);
 
   const disabled = useMemo(() => {
-    const items = Object.values(checked);
     return items.every((item) => !checked[item.id]);
-  }, [checked]);
+  }, [items]);
+
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   return (
-    <Button
-      variant="destructive"
-      disabled={disabled}
-      className="cursor-pointer"
-      onClick={handleDelete}
-    >
-      <div className="flex gap-1 items-center uppercase"> Delete Employees</div>
-    </Button>
+    <Dialog onOpenChange={setOpen} open={open}>
+      <DialogTrigger
+        disabled={disabled}
+        className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90 uppercase px-4"
+      >
+        Delete Employees
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you sure?</DialogTitle>
+          <DialogDescription>
+            Do you really want to delete theese records? This process cannot be
+            undone.
+          </DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter>
+          <Button onClick={handleDelete} variant="destructive">
+            Delete {items.length} items
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
